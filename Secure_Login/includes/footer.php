8532899042d2bb8216fe6b416e107d4a683ad3c6
Nick McMahon <nickmcm92@gmail.com>
2013-05-23 14:41:51 +1000

    Initial push

--- Secure_Login/includes/footer.php
+++
@@ -1,8 +0,0 @@
-		<footer>
-		 <p class="copyright">
-		 	&copy;  <?php echo "Swinburne Group 3"; ?> 2013
-		 </p>
-		</footer>
-	</div>
-	</body>
-</html>
--- Secure_Login/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("config/config.php");
-include_once("includes/Router.php");
-
-Router::route();
-
-?>
